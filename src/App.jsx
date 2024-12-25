ea179d4463c5803ae9d4a275ce81d3b5d3cf1952
Chamika Jayarathna <failgames.lk@gmail.com>
2024-12-25 17:26:14 +0530

    create HomeCards componet and Card componnet

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
+import HomeCards from './components/HomeCards';
 
 const App = () => {
   return (
     <div className="">
       <NavBar/>
       <Hero title="Become a React Dev" subtitle="Find the React job that fits your skills and needs"/>
+      <HomeCards/>
     </div>
   );
 }
