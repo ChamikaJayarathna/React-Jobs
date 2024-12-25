0a07ce7e0c32a68651d690fb714363614fc32bbf
Chamika Jayarathna <failgames.lk@gmail.com>
2024-12-25 16:30:16 +0530

    create Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+import Hero from './components/Hero';
 
 const App = () => {
   return (
     <div className="">
       <NavBar/>
+      <Hero title="Become a React Dev" subtitle="Find the React job that fits your skills and needs"/>
     </div>
   );
 }
