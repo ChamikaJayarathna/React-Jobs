de8b08b351b52d9efed76768eb66b862dde3fb8e
Chamika Jayarathna <failgames.lk@gmail.com>
2024-12-27 11:06:18 +0530

    update all job pages and component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,8 +3,10 @@ import logo from "../assets/images/logo.png";
 import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-
-  const linkClass = ({ isActive }) => isActive ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
+  const linkClass = ({ isActive }) =>
+    isActive
+      ? "bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+      : "text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2";
 
   return (
     <nav className="bg-indigo-700 border-b border-indigo-500">
@@ -19,22 +21,13 @@ const NavBar = () => {
             </NavLink>
             <div className="md:ml-auto">
               <div className="flex space-x-2">
-                <NavLink
-                  to="/"
-                  className={linkClass}
-                >
+                <NavLink to="/" className={linkClass}>
                   Home
                 </NavLink>
-                <NavLink
-                  to="/jobs"
-                  className={linkClass}
-                >
+                <NavLink to="/jobs" className={linkClass}>
                   Jobs
                 </NavLink>
-                <NavLink
-                  to="/add-job"
-                  className={linkClass}
-                >
+                <NavLink to="/add-job" className={linkClass}>
                   Add Job
                 </NavLink>
               </div>
