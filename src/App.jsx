b0f7941c60b5aab8739be97c282fd0b06c3186de
Chamika Jayarathna <failgames.lk@gmail.com>
2024-12-27 09:32:23 +0530

    update navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { createBrowserRouter, createRoutesFromChildren, Route, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
 
 const router = createBrowserRouter(
   createRoutesFromChildren(
     <Route path='/' element={<MainLayout/>} >
         <Route index element={<HomePage/>} />
+        <Route path='/jobs' element={<JobsPage/>}/>
     </Route>
   )
 );
